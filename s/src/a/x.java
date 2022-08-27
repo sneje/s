d505a10b70efe23016f02ee7163dd4b369c59e1d
DELL <DELL@DESKTOP-36R32J1>
2022-08-27 22:52:49 +0530

    s

---
+++ s/src/a/x.java
@@ -0,0 +1,15 @@
+package a;
+
+import org.testng.annotations.Test;
+
+public class x {
+	@Test
+	public void test1() {
+		//System.out.println("1st is running");
+	}
+	@Test
+	public void atestcase2() {
+		System.out.println("2nd is running");
+	}
+
+}
